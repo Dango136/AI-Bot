6436d9dedefcfb1397a8d4cb258b009312de13ac
Dango136 <sonic143345@gmail.com>
2026-05-14 20:40:16 +0700

    first commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+from config import token
+import telebot
+from telebot.types import (
+    ReplyKeyboardMarkup, 
+    KeyboardButton, 
+    WebAppInfo,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+WEB_URL = "https://pytelegrambotminiapp.vercel.app"
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot(token)
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+# Запуск бота
+@bot.message_handler(commands=["start_mini_app"])
+def start(message):
+    reply_keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    reply_keyboard_markup.row(KeyboardButton("Start MiniApp", web_app=WebAppInfo(WEB_URL)))
+
+    inline_keyboard_markup = InlineKeyboardMarkup()
+    inline_keyboard_markup.row(InlineKeyboardButton('Start MiniApp', web_app=WebAppInfo(WEB_URL)))
+
+    bot.reply_to(message, "Click the bottom inline button to start MiniApp", reply_markup=inline_keyboard_markup)
+    bot.reply_to(message, "Click keyboard button to start MiniApp", reply_markup=reply_keyboard_markup)
+
+@bot.message_handler(content_types=['web_app_data'])
+def web_app(message):
+    bot.reply_to(message, f'Your message is "{message.web_app_data.data}"')
+@bot.message_handler(content_types=['photo'])
+def photo(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = file_info.file_path.split('/')[-1]
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+bot.infinity_polling()
